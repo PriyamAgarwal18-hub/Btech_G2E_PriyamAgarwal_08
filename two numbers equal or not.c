38f46e403fbc1ec1989b2f1363873f46a3e12e40
PriyamAgarwal18-hub <25116cn347@glbitm.ac.in>
2026-02-24 14:25:28 +0530

    Create two numbers equal or not.c
    Program to check whether the two numbers entered by the user are equal or not

---
+++ two numbers equal or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter 2 numbers to be checked:");
+scanf("%d%d",&a,&b);
+if(a=b)
+{
+printf("The entered numbers are equal");
+}
+else
+{
+printf("The entered numbers are not equal");
+}
+return 0;
+}
