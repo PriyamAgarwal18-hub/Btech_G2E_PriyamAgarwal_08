420b2644189d2ef6f8b9a947a760927090930813
PriyamAgarwal18-hub <25116cn347@glbitm.ac.in>
2026-02-24 14:08:03 +0530

    Create Celsius to Fahrenheit.c
    Program to convert temperature from Degree Celsius to Degree Fahrenheit

---
+++ Celsius to Fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float C,F;
+printf("Enter the temperature in C");
+scanf("%f",&C);
+F = 32 + 9*C/5;
+printf("The converted temperature in F is: %f",F);
+return 0;
+}
