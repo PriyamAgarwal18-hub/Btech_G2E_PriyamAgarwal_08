79834f5e6df785a7b97c6648ec455e77c6bb268f
PriyamAgarwal18-hub <25116cn347@glbitm.ac.in>
2026-03-17 02:14:40 -0700

    Create wap that inputs 2 arrays and saves sum of corresponding elements of these arrays in a third array & prints them.c
    wap that inputs 2 arrays and saves sum of corresponding elements of these arrays in a third array & prints them.

---
+++ wap that inputs 2 arrays and saves sum of corresponding elements of these arrays in a third array & prints them.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,n,arr1[n],arr2[n],arr3[n],arr[i];
+printf("Enter the number of elements in array:");
+printf("Enter the first array:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+printf("Enter the second array:\n");
+for(i=0;i<n;i++)
+{
+arr3[i]=arr1[i]+arr2[i];
+printf("%d",arr3[i]);
+}
+return 0;
+}
